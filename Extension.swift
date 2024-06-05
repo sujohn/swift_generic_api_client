90881c2eed8210e5b3a6cf394c8b5507bc56b7c3
Shaiful Sujohn <sujohn.sust.cse@gmail.com>
2024-06-05 16:24:54 +0600

    Create Extension.swift

---
+++ Extension.swift
@@ -0,0 +1,7 @@
+extension Encodable {
+
+    var toDictionary: [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
+    }
+}
