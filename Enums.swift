4c761e6162f27603dfe633cc2d492a7fab116665
Shaiful Sujohn <sujohn.sust.cse@gmail.com>
2024-06-05 16:33:32 +0600

    Update Enums with FileType

--- Enums.swift
+++ Enums.swift
@@ -6,3 +6,20 @@ enum RequestMethod: String {
     case post = "POST"
     case put = "PUT"
 }
+
+enum FileType: String {
+    /// image/webm, audio/ogg, audio/mpeg, audio/mp4, video/mpeg, video/quicktime, video/webm
+    /// application/msword, application/excel, application/powerpoint, application/x-zip
+    case jpeg = "image/jpeg"
+    case png = "image/png"
+    case gif = "image/gif"
+    case tiff = "image/tiff"
+    case bmp = "image/bmp"
+    case quickTime = "video/quicktime"
+    case mov = "video/mov"
+    case mp4 = "video/mp4"
+    case pdf = "application/pdf"
+    case vnd = "application/vnd"
+    case plainText = "text/plain"
+    case anyBinary = "application/octet-stream"
+}
