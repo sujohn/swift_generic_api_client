21c475e26084ad9b46d06c63462622f127b14e91
Shaiful Sujohn <sujohn.sust.cse@gmail.com>
2024-06-05 16:31:03 +0600

    Event example view model added.

--- Examples.swift
+++ Examples.swift
@@ -74,3 +74,50 @@ enum UserEndpoints: EndpointProvider {
         }
     }
 }
+
+//=================================================
+
+final class EventsViewModel: ObservableObject {
+
+    let apiClient: ApiProtocol // 1
+
+    init(apiClient: ApiProtocol = ApiClient()) { // MockApiClient()
+         self.apiClient = apiClient // 2
+     }
+
+    @Published var userEvents: [Event] = []
+    @Published var eventError: ApiError?
+    private var cancellables: Set<AnyCancellable> = []
+
+    @MainActor
+    func getAsyncEvents() async { // 3
+        let endpoint = EventsEndpoints.getEvents
+        Task.init {
+            do {
+                let events = try await apiClient.asyncRequest(endpoint: endpoint, responseModel: [Event].self)
+                userEvents = events
+            } catch let error as ApiError {
+                eventError = error
+            }
+        }
+    }
+
+    func getCombineEvents() { // 4
+        let endpoint = EventsEndpoints.getEvents
+        apiClient.combineRequest(endpoint: endpoint, responseModel: [Event].self)
+            .receive(on: DispatchQueue.main) // 5
+            .sink { [weak self] completion in
+                guard let self = self else { return }
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    self.eventError = error
+                }
+            } receiveValue: { [weak self] events in
+                guard let self = self else { return }
+                self.userEvents = events
+            }
+            .store(in: &cancellables)
+    }
+}
